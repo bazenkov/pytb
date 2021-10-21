2fba75c481532cfeaac45f900fad1189ee940957
bazenkov <n.bazenkov@yandex.ru>
2021-10-21 12:26:33 +0300

    Changes to upload_arxiv.py - matchdate argument is added. It uploads only log files with names starting with YYYYmmdd.

--- upload_arxiv.py
+++ upload_arxiv.py
@@ -3,6 +3,7 @@ import os
 from sys import stderr
 from os import path
 import json
+import datetime as dt
 from datetime import datetime
 import time
 import argparse
@@ -31,13 +32,13 @@ def from_js_timestamp(js_timestamp):
 MAX_TRY = 5
 
 
-def upload(tb_url, data, start_ts, end_ts, delay):
+def upload(tb_url, data, starttime, endtime, delay):
     """data is json array:
     [{"ts": 1616965289148, "devEui": "MOXAKON1-MR234-017", "values": {"PT": 0,...} }, {...}, ... ]
     """
 
     for entry in data:
-        if start_ts <= from_js_timestamp(entry['ts']) < end_ts:
+        if starttime.timestamp() <= from_js_timestamp(entry['ts']) < endtime.timestamp():
             device_token = entry['devEui']
             message_json = entry
             del message_json['devEui']
@@ -55,12 +56,6 @@ def upload(tb_url, data, start_ts, end_ts, delay):
             time.sleep(delay / DELAY_MS)
 
 
-# TEST_DEVICES = {'MERCURY-0':'aceb69f0-a599-11ea-9c42-6b68c14fd0ab',
-# 'MERCURY-2':'cdeb3840-a59b-11ea-9c42-6b68c14fd0ab'} def test_upload(tb_url, data, devices=TEST_DEVICES): '''Test
-# that every entry in the data is present in TB devices - dict {'device_token':'device_id'} ''' for d in devices:
-# ts_data = tb.ge
-
-
 def get_args():
     parser = argparse.ArgumentParser(
         description="Uploads collector arxiv to Thingsboard. \
@@ -68,17 +63,21 @@ def get_args():
     parser.add_argument('--format', help="expand or pack", default="pack")
     parser.add_argument('--delay', help="Time in milliseconds between messages.", default=DELAY_MS, type=int)
     parser.add_argument('--start', help="The time of the beginning of the target period (including). \
-                        String in ISO format as \"2021-01-25 09:01:00\" ", default='1970-01-02 12:00:00')
+                        String in ISO format as \"2021-01-25 09:01:00\" ", default='1970-01-02 12:00:00',
+                        type=datetime.fromisoformat)
     parser.add_argument('--end', help="The time of the end of the target period (excluding). \
-                        String in ISO format as \"2021-01-25 09:01:00\" ", default='2999-01-01 00:00:00')
-    # parser.add_argument("--match", help="The pattern of the log files names")
+                        String in ISO format as \"2021-01-25 09:01:00\" ", default='2999-01-01 00:00:00',
+                        type=datetime.fromisoformat)
+    parser.add_argument("--matchdate", help="Only files that match YYYYmmdd pattern with the date belonging to  \
+                        the range given by --start and --end arguments. Used only input is a directory.",
+                        action="store_true")
     parser.add_argument('url', help="TB host url")
     parser.add_argument('input', help="File with json data to upload.")
     return parser.parse_args()
 
 
-def get_ts(timestr):
-    return datetime.fromisoformat(timestr).timestamp()
+def get_ts(dt_obj):
+    return dt_obj.timestamp()
 
 
 def is_gzip(filename):
@@ -113,7 +112,7 @@ def upload_file(filename, url, fileformat, starttime, endtime, delay):
     def upload_expand():
         data = json.loads(make_json(filename))
         print(f"Uploading data from {starttime} to {endtime}...")
-        upload(url, data, get_ts(starttime), get_ts(endtime), delay)
+        upload(url, data, starttime, endtime, delay)
 
     def upload_pack():
         with open_gz(filename) as f:
@@ -123,7 +122,7 @@ def upload_file(filename, url, fileformat, starttime, endtime, delay):
                     data = json.loads(line[:-2])
                 else:
                     data = json.loads(line)
-                upload(url, data, get_ts(starttime), get_ts(endtime), delay)
+                upload(url, data, starttime, endtime, delay)
 
     print(f"Reading {filename}...")
     if fileformat == "expand":
@@ -137,14 +136,55 @@ def is_log(filename):
     return filename[-3:] == "log" or filename[-6:] == "log.gz"
 
 
+TZ_MOSCOW = dt.timezone(dt.timedelta(hours=3), "UTC+03:00")
+
+
+def match_date(filename, starttime, endtime):
+    """ Match the filename to the pattern YYYYmmdd*
+    Return True if the date YYYYmmdd in the filename is between starrtime and endtime, ends included.
+
+    >>> match_date("20210927-anytext.extension", datetime(2021,9,25), datetime(2021,9,27))
+    True
+    >>> match_date("Anytext.extension", datetime(2021,9,25), datetime(2021,9,27))
+    False
+    >>> match_date("20210927-anytext.extension", datetime(2021,9,24), datetime(2021,9,26))
+    False
+    """
+    def get_date(dtobj):
+        return datetime(dtobj.year, dtobj.month, dtobj.day, tzinfo=dtobj.tzinfo)
+
+    if len(filename) < 8:
+        return False
+    else:
+        try:
+            fdate = datetime.strptime(filename[0:8], "%Y%m%d")
+            fdate = fdate.astimezone(TZ_MOSCOW)
+        except ValueError as e:
+            return False
+        return get_date(starttime) <= fdate <= get_date(endtime)+dt.timedelta(days=1)
+
+
 def main(args):
+    def list_files():
+        f_list = [f.path for f in os.scandir(args.input) if is_log(f.name)]
+        if args.matchdate:
+            f_list = [f for f in f_list if match_date(path.basename(f), args.start, args.end)]
+        return sorted(f_list, key=lambda x: os.stat(x).st_mtime)
+
     if path.isdir(args.input):
         print(f"The input directory is {args.input}")
-        files = sorted([f.path for f in os.scandir(args.input) if is_log(f.name)], key=lambda x: os.stat(x).st_mtime)
-        for f in files:
-            upload_file(f, args.url, args.format, args.start, args.end, args.delay)
+        files = list_files()
+        nl = '\n'
+        print(f"Files selected for upload:\n {nl.join(files)}")
+        for fname in files:
+            upload_file(fname, args.url, args.format, args.start, args.end, args.delay)
     else:
-        upload_file(args.input, args.url, args.format, args.start, args.end, args.delay)
+        upload_file(args.input,
+                    args.url,
+                    args.format,
+                    args.start,
+                    args.end,
+                    args.delay)
 
 
 if __name__ == "__main__":
